007c84afeca275eb3654305bd5c619c4ac3c91d9
Egor Kharlamov <egor@kharlamov.co.uk>
2024-07-19 14:06:05 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import logo from './assets/images/Logo.png';
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo" alt="FateLock Logo" />
+      </div>
+      <h1>Work in Progress</h1>
+      <div className="card">
+        <p>
+          This page is currently under construction. Check back soon for updates!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
