a00ce55d083c17a50dd7a000e10544b3d2edda7f
Egor Kharlamov <egor@kharlamov.co.uk>
2024-08-02 17:04:33 +0100

    Added google analytics support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-  process.env.NODE_ENV == "production" ? vitePluginFaviconsInject('src/assets/images/logo.png') : false
+  process.env.NODE_ENV == "production" ? vitePluginFaviconsInject('src/assets/images/logo.png') : false,
+  VitePluginRadar({
+    // Google Analytics tag injection
+    analytics: {
+      id: 'G-67ECDV25M9',
+    },
+  })
   ],
   server: {
     watch: {
